112cc099bbdb443f43be5ad70a788e7dd3b3742a
Nhat Thanh <thanhdntage@gmail.com>
2018-06-10 00:20:38 +0700

    when sleep

---
+++ 2_constructor.js
@@ -0,0 +1,10 @@
+// constructor
+function Book() {
+  console.log('Book Initialized..');
+}
+
+// Book(); // result: Book Initialized..
+
+// const book = new Book();
+// const book2 = new Book();
+//TODO: going to sleep
